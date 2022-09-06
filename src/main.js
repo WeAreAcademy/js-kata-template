d94c62968ea43bbf543a692c7c4af9748d84d26e
nbogie <neillbogie@googlemail.com>
2022-09-06 22:36:44 +0100

    ready to export more than one fn

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const sum = require("./sum");
+const { sum } = require("./sum");
+
+console.log("sum is ", sum(10, 20));
 console.log("This is main.js.");
 console.log("You should probably use `yarn test` or `yarn test --watchAll`");
-console.log("sum is ", sum(10, 20));
