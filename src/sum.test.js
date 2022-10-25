422065d9dd7e09a53bec80b0e07c28bfa057c13b
nbogie <neillbogie@googlemail.com>
2022-10-25 09:15:36 +0100

    remove spilled text from sum.test.js

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,7 +3,7 @@ const { sum } = require("./sum");
 // jest docs for expect "matchers"
 // https://jestjs.io/docs/expect
 
-https: test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
