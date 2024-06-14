2cf0d537c964368efdfcbdd2f42a5897890957d2
2405-007-dk <danakengessova@gmail.com>
2024-06-14 13:42:41 +0100

    final changes

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { sum } from "./sum.js";
-
-console.log("sum is ", sum(10, 20));
-console.log("This is main.js.");
-console.log("You should probably use `yarn test` or `yarn test --watchAll`");
